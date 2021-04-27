e11e4b8c6658dfa08ad424d64cfaee15dae7e32a
chu <akdlgusdnr26@gmail.com>
2021-04-27 19:07:16 +0900

    update

--- front/pages/login.tsx
+++ front/pages/login.tsx
@@ -3,6 +3,9 @@ import LoginForm from 'components/loginForm';
 import { useSelector } from 'react-redux';
 import { RootState } from 'slices';
 import { useRouter } from 'next/router';
+import wrapper from 'store/configureStore';
+import axios from 'axios';
+import { loadUser } from 'actions/user';
 
 const LogIn = () => {
   const { user } = useSelector((state: RootState) => state.user);
@@ -20,5 +23,16 @@ const LogIn = () => {
     </>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  // 쿠키가 브라우저에 있는경우만 넣어서 실행
+  // (주의, 아래 조건이 없다면 다른 사람으로 로그인 될 수도 있음)
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  await context.store.dispatch(loadUser());
+});
 
 export default LogIn;
