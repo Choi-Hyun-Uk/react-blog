87a244741ef90d09d28797dac3b14433674db085
chu <akdlgusdnr26@gmail.com>
2021-04-29 17:31:47 +0900

    lambda index update

--- lambda/index.js
+++ lambda/index.js
@@ -5,7 +5,7 @@ const sharp = require('sharp');
 const s3 = new AWS.S3();
 
 // event - S3에서 작업 진행 
-exports.handler = async (event, context, callback) => {
+exports.imageHandler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name; // react-blog-s3
   const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12312312_abc.png
   console.log('Bucket',Bucket, 'Key', Key);
