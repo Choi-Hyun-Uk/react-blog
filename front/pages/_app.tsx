22c70e5a2b769d17b511175e6adbf83807087d5e
chu <akdlgusdnr26@gmail.com>
2021-04-30 14:06:32 +0900

    app update

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -9,9 +9,32 @@ const App: FC<any> = ({ Component, pageProps }) => {
   const { singlePost } = useSelector((state: RootState) => state.post);
 
   // 정규표현식 - url 타이틀 특수문자 및 공백 '-'변환 후 마지막 '-'은 제거
-  const regex = /[\s\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]+/g;
-  const regex2 = singlePost?.title.toString().replace(regex, '-');
-  const title = regex2.replace(/-$/, '');
+  if (singlePost) {
+    const regex = /[\s\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]+/g;
+    const regex2 = singlePost.title.replace(regex, '-');
+    const title = regex2.replace(/-$/, '');
+    return (
+      <>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+          />
+          <meta name="description" content={singlePost.content} />
+          <meta property="og:title" content={`${singlePost.User.nickname}님의 게시글`} />
+          <meta
+            property="og:description"
+            content={singlePost.Images[0] ? singlePost.Images[0].src : 'http://chudevlog.com/favicon.ico'}
+          />
+          <meta property="og:url" content={`http://chudevlog.com/${singlePost.User.nickname}/${title}`} />
+          <link rel="icon" href="/favicon.ico"></link>
+          <title>My Blog</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
+  }
 
   return (
     <>
@@ -21,13 +44,6 @@ const App: FC<any> = ({ Component, pageProps }) => {
           name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
         />
-        <meta name="description" content={singlePost?.content} />
-        <meta property="og:title" content={`${singlePost?.User.nickname}님의 게시글`} />
-        <meta
-          property="og:description"
-          content={singlePost?.Images[0] ? singlePost.Images[0].src : 'http://chudevlog.com/favicon.ico'}
-        />
-        <meta property="og:url" content={`http://chudevlog.com/${singlePost?.User.nickname}/${singlePost && title}`} />
         <link rel="icon" href="/favicon.ico"></link>
         <title>My Blog</title>
       </Head>
