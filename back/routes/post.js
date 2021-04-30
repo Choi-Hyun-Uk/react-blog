6d145a47716bcae0746702edd5ccf8ca6d85cf53
chu <akdlgusdnr26@gmail.com>
2021-04-30 23:07:13 +0900

    replace rm

--- back/routes/post.js
+++ back/routes/post.js
@@ -148,6 +148,8 @@ router.patch('/', isLoggedIn, upload.none(), async (req, res, next) => {
                 );
                 await post.addImages(images);
 
+                console.log('images', images);
+
                 return res.status(200).json({
                     postId: req.body.postId,
                     title: req.body.title,
@@ -157,6 +159,8 @@ router.patch('/', isLoggedIn, upload.none(), async (req, res, next) => {
             } else { // 이미지 한 개 업로드 시 - image: '1.png'
                 const image = await Image.create({ src: req.body.image });
                 await post.addImages(image); // 해당 포스트 전체 내용
+
+                console.log('image', image);
 
                 res.status(200).json({
                     postId: req.body.postId,
