08941ff8ddb4a02c820b6ebb08ef2bddfbc97440
chu <akdlgusdnr26@gmail.com>
2021-05-06 18:52:56 +0900

    searchPost postImage update

--- front/components/search/searchPost.tsx
+++ front/components/search/searchPost.tsx
@@ -41,7 +41,7 @@ const SearchPost = ({ post }) => {
         <div className="post-desc">
           {post.Images.length > 0 && (
             <div className="image">
-              <img src={post.User.Images[0].src} />
+              <img src={post.Images[0].src} />
             </div>
           )}
           <p>{post.content}</p>
